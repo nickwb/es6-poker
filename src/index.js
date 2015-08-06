5c32161c22af0932d460a4d81a641b748010cc35
Nick Young <nick@nickwb.net>
2015-08-06 23:28:26 +1000

    Just focus on better hands for now...

--- src/index.js
+++ src/index.js
@@ -8,21 +8,21 @@ import Deal from "./deal";
 import Round from "./round";
 import Deck from "./deck";
 
-{
-	let deal = Deal.random(6);
-	_.each(deal.players, p => console.log(`${p.name} has ${p.pocket}.`));
-
-	console.log(`The community cards are ${deal.communityCards.join(', ')}.`);
-
-	let winners = deal.getWinners();
-	if(winners.length == 1) {
-		let w = winners[0];
-		console.log(`The winner is ${w.player.name} with a ${w.bestHand.score()}. ${w.bestHand}`);
-	} else {
-		console.log(`There is a tie between: ${_.map(winners, w => w.player.name).join(', ')}.`);
-		_.each(winners, w => console.log(`${w.player.name} has ${w.bestHand}. ${w.bestHand.score()}.`));
-	}
-}
+// {
+// 	let deal = Deal.random(6);
+// 	_.each(deal.players, p => console.log(`${p.name} has ${p.pocket}.`));
+//
+// 	console.log(`The community cards are ${deal.communityCards.join(', ')}.`);
+//
+// 	let winners = deal.getWinners();
+// 	if(winners.length == 1) {
+// 		let w = winners[0];
+// 		console.log(`The winner is ${w.player.name} with a ${w.bestHand.score()}. ${w.bestHand}`);
+// 	} else {
+// 		console.log(`There is a tie between: ${_.map(winners, w => w.player.name).join(', ')}.`);
+// 		_.each(winners, w => console.log(`${w.player.name} has ${w.bestHand}. ${w.bestHand.score()}.`));
+// 	}
+// }
 
 {
 	let d = Deck.random();
@@ -41,10 +41,10 @@ import Deck from "./deck";
 
 	_.each(better, b => console.log(`${b}: ${b.score()}`));
 
-	console.log('The following improved hands are possible:');
-	let improved = r.getPlayerImprovedHands(2);
-	_.each(improved, i => {
-		let best = i.bestMember;
-		console.log(`${best}: ${best.score()}. (${i.numMembers} variants)`);
-	});
+	// console.log('The following improved hands are possible:');
+	// let improved = r.getPlayerImprovedHands(2);
+	// _.each(improved, i => {
+	// 	let best = i.bestMember;
+	// 	console.log(`${best}: ${best.score()}. (${i.numMembers} variants)`);
+	// });
 }
